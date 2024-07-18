704e3ccb7c09f6b1e804082e8ac3894a71852017
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2024-07-18 20:52:12 +0200

    Non-static entry point

--- php/entry_point.php
+++ php/entry_point.php
@@ -8,7 +8,8 @@ require __DIR__ . '/vendor/autoload.php';
 
 $filename = $argc > 1 ? $argv[1] : ROOT_DIR . DIRECTORY_SEPARATOR . "resources" . DIRECTORY_SEPARATOR . "strange_characters.json";
 echo "Using character file: $filename" . PHP_EOL;
-CharacterDataParser::initWithDataFrom($filename);
+//CharacterDataParser::initWithDataFrom($filename);
+$parser = new CharacterDataParser($filename);
 
 while (true) {
     echo "Please enter the path to evaluate: (type 'exit' to exit program)".PHP_EOL;
@@ -18,7 +19,7 @@ while (true) {
     if ("exit" === $path) {
         break;
     }
-    $character = CharacterDataParser::findCharacterBy($path);
+    $character = $parser->findByPath($path);
 
     echo "Character: $character".PHP_EOL;
 }
